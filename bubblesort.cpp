a2e43a6ab743c633441dae24a45ec783b1bfde88
CaQtiml <37920301+CaQtiml@users.noreply.github.com>
2018-11-20 22:15:06 +0700

    Add files via upload

---
+++ bubblesort.cpp
@@ -0,0 +1,28 @@
+#include "bits/stdc++.h"
+using namespace std;
+int a[100];
+template<typename T>
+void bubblesort(T a[] , T n)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-1-i;j++)
+        {
+            if(a[j]>a[j+1]) swap(a[j],a[j+1]);
+        }
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    for(int i=0;i<n;i++)
+    {
+        cin >> a[i];
+    }
+    bubblesort(a,n);
+    for(int i=0;i<n;i++)
+    {
+        cout << a[i] << "\n";
+    }
+}
